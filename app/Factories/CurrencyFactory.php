1b0d9c10c533371c6cb6ecdd107a2944d5705ca2
Sebastien Rodrigue <sebastien@imarcom.net>
2018-01-07 20:18:31 -0500

    fix currency factory

--- app/Factories/CurrencyFactory.php
+++ app/Factories/CurrencyFactory.php
@@ -22,15 +22,28 @@ class CurrencyFactory extends Factory
             $data = CurrenciesUpdater::findCurrency($symbol);
             
             $currency = new Currency();
-            $currency->name = $data['name'];
-            $currency->symbol = $data['symbol'];
-            $currency->api_path = $data['id'];
-            $currency->usd_value = $data['price_usd'];
-            $currency->cad_value = $data['price_cad'];
-            $currency->btc_value = $data['price_btc'];
-            $currency->percent_change_1h = $data['percent_change_1h'];
-            $currency->percent_change_24h = $data['percent_change_24h'];
-            $currency->percent_change_7d = $data['percent_change_7d'];
+            $currency->symbol = $symbol;
+
+            if (is_null($data)) {
+                $currency->name = $symbol;
+                $currency->api_path = 'UNDEFINED';
+                $currency->usd_value = 0;
+                $currency->cad_value = 0;
+                $currency->btc_value = 0;
+                $currency->percent_change_1h = 0;
+                $currency->percent_change_24h = 0;
+                $currency->percent_change_7d = 0;
+            } else {
+                $currency->name = $data['name'];
+                $currency->api_path = $data['id'];
+                $currency->usd_value = $data['price_usd'];
+                $currency->cad_value = $data['price_cad'];
+                $currency->btc_value = $data['price_btc'];
+                $currency->percent_change_1h = $data['percent_change_1h'];
+                $currency->percent_change_24h = $data['percent_change_24h'];
+                $currency->percent_change_7d = $data['percent_change_7d'];
+            }
+
             $currency->description = '';
             $currency->save();
         }
